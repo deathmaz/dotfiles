c1142ff4f1e61d7bef6c0716cd19c6f4917c5a60
deathmaz <00maz1987@gmail.com>
2023-09-24 15:20:47 +0300

    feat(scripts): change reverse logic

--- deathmaz/executable/scripts/bin/create-m3u-from-url.sh
+++ deathmaz/executable/scripts/bin/create-m3u-from-url.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-yt-dlp --skip-download --flat-playlist --print "%()j" $1 \
-  | jq -r \
-  '"#EXTINF:" + (.duration | tostring) + "," + .channel + " - " + .title + "\n" + .url' > res.m3u
+# yt-dlp --skip-download --flat-playlist --print "%()j" "$1" \
+#   | jq -r 'reverse | .[] |
+#   if .duration == null then
+#     "#EXTINF:0," + .channel + " - " + .title + "\n" + .url
+#   else
+#     "#EXTINF:" + (.duration / 60 | floor | tostring) + "," + .channel + " - " + .title + "\n" + .url
+#   end
+#   ' > res.m3u
+
+tmp_file=$(mktemp)
+
+yt-dlp --skip-download --flat-playlist --print "%()j" "$1" > "$tmp_file"
+
+tac "$tmp_file" | jq -r '
+  if .duration == null then
+    "#EXTINF:0," + .channel + " - " + .title + "\n" + .url
+  else
+    "#EXTINF:" + (.duration / 60 | round | tostring) + "," + .channel + " - " + .title + "\n" + .url
+  end
+' > res.m3u
+
+rm "$tmp_file"
