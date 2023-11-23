6d9832c58d80f5852ff060089a13e261030de5ff
deathmaz <00maz1987@gmail.com>
2023-11-23 22:58:24 +0200

    refactor: don't hardcode mpv scripts

--- deathmaz/executable/scripts/bin/update-manually-installed.sh
+++ deathmaz/executable/scripts/bin/update-manually-installed.sh
@@ -6,3 +6,4 @@ source $HOME/dotfiles/deathmaz/executable/scripts/bin/install-yt-dlp.sh
 source $HOME/dotfiles/deathmaz/executable/scripts/bin/download-ffmpeg-for-yt-dlp.sh
 source $HOME/dotfiles/deathmaz/executable/scripts/bin/install-kitty.sh
 source $HOME/dotfiles/deathmaz/executable/scripts/bin/install-fast-syntax-highlighting.sh
+source $HOME/dotfiles/deathmaz/executable/scripts/bin/install-mpv-scripts
