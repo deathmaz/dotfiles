9e3e7a2089f0ea4b8d040ab213c5157c9597ee66
deathmaz <00maz1987@gmail.com>
2024-03-20 22:32:37 +0200

    chore: switch to harpoon2

--- deathmaz/nvim/.config/nvim/lua/_plugins.lua
+++ deathmaz/nvim/.config/nvim/lua/_plugins.lua
@@ -225,18 +225,19 @@ return {
   {
     'ThePrimeagen/harpoon',
     event = 'VeryLazy',
+    branch = "harpoon2",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
     config = function()
-      require("harpoon").setup({
-        menu = {
-          width = 80,
-        }
-      })
-      vim.keymap.set('n', '<leader>ha', require("harpoon.mark").add_file)
-      vim.keymap.set('n', '<leader>hm', require("harpoon.ui").toggle_quick_menu)
-      vim.keymap.set('n', '<leader>h1', function() require("harpoon.ui").nav_file(1) end)
-      vim.keymap.set('n', '<leader>h2', function() require("harpoon.ui").nav_file(2) end)
-      vim.keymap.set('n', '<leader>h3', function() require("harpoon.ui").nav_file(3) end)
-      vim.keymap.set('n', '<leader>h4', function() require("harpoon.ui").nav_file(4) end)
+      local harpoon = require("harpoon")
+      harpoon:setup();
+      vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>hm", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
     end,
     enabled = true,
   },
