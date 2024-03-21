fe8af74ceff8f34cc28945f92d48628ec329e689
deathmaz <00maz1987@gmail.com>
2024-03-21 17:51:55 +0200

    feat(nvim): set default width of Git screen to 50%

--- deathmaz/nvim/.config/nvim/lua/_fugitive.lua
+++ deathmaz/nvim/.config/nvim/lua/_fugitive.lua
@@ -1,9 +1,13 @@
-vim.keymap.set('', '\\gs', function() vim.cmd.Git({
-  mods = {
-    vertical = true,
-    split = 'topleft',
-  },
-}) end)
+vim.keymap.set('', '\\gs', function()
+  vim.cmd.Git({
+    mods = {
+      vertical = true,
+      split = 'topleft',
+    },
+  })
+  local width = math.floor(vim.o.columns * 0.5)
+  vim.cmd("vertical resize " .. width)
+end)
 
 -- Makes it possible for `:GBrowse` to work with netrw plugin disabled
 -- The idea was taken from https://github.com/kyazdani42/nvim-tree.lua/issues/559#issuecomment-962593611
