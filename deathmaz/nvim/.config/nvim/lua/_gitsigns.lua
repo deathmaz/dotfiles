0cfac0537950b7215eeda5905fd68f7d92d5aa81
deathmaz <00maz1987@gmail.com>
2024-03-30 21:11:44 +0200

    feat(nvim): fzf-lua extension for gitsigns

--- deathmaz/nvim/.config/nvim/lua/_gitsigns.lua
+++ deathmaz/nvim/.config/nvim/lua/_gitsigns.lua
@@ -6,10 +6,61 @@ end
 local icons = require('_icons')
 gitsigns.setup {
   signs = {
-    add          = {hl = 'GitSignsAdd'   , text = icons.box.LightVertical, numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = icons.box.LightVertical, numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = icons.box.HeavyHorizontal, numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    add          = {
+      hl = 'GitSignsAdd',
+      text = icons.box.LightVertical,
+      numhl = 'GitSignsAddNr',
+      linehl = 'GitSignsAddLn',
+    },
+    change       = {
+      hl = 'GitSignsChange',
+      text = icons.box.LightVertical,
+      numhl = 'GitSignsChangeNr',
+      linehl = 'GitSignsChangeLn',
+    },
+    delete       = {
+      hl = 'GitSignsDelete',
+      text = icons.box.HeavyHorizontal,
+      numhl = 'GitSignsDeleteNr',
+      linehl = 'GitSignsDeleteLn',
+    },
+    topdelete    = {
+      hl = 'GitSignsDelete',
+      text = '‾',
+      numhl = 'GitSignsDeleteNr',
+      linehl = 'GitSignsDeleteLn',
+    },
+    changedelete = {
+      hl = 'GitSignsChange',
+      text = '~',
+      numhl = 'GitSignsChangeNr',
+      linehl = 'GitSignsChangeLn',
+    },
   },
 }
+
+vim.keymap.set("n", "]h", function() gitsigns.next_hunk() end)
+vim.keymap.set("n", "[h", function() gitsigns.prev_hunk() end)
+
+local fzf_lua = require 'fzf-lua'
+vim.keymap.set('n', '<space>g',
+  function()
+    local actions = gitsigns.get_actions()
+    fzf_lua.fzf_exec(function(cb)
+        for k, _ in pairs(actions) do
+          cb(k)
+        end
+        cb()
+      end,
+      {
+        actions = {
+          ['default'] = function(selected)
+            gitsigns[selected[1]]()
+          end,
+        },
+        winopts = {
+          height = 0.6,
+          width = 0.6,
+        }
+      })
+  end)
