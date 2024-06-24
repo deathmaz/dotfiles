02adf704c5eedcf2daf87fccecd39250514014d4
deathmaz <00maz1987@gmail.com>
2024-06-24 22:58:38 +0300

    refactor(nvim): remove deprecated options

--- deathmaz/nvim/.config/nvim/lua/_gitsigns.lua
+++ deathmaz/nvim/.config/nvim/lua/_gitsigns.lua
@@ -7,34 +7,19 @@ local icons = require('_icons')
 gitsigns.setup {
   signs = {
     add          = {
-      hl = 'GitSignsAdd',
       text = icons.box.LightVertical,
-      numhl = 'GitSignsAddNr',
-      linehl = 'GitSignsAddLn',
     },
     change       = {
-      hl = 'GitSignsChange',
       text = icons.box.LightVertical,
-      numhl = 'GitSignsChangeNr',
-      linehl = 'GitSignsChangeLn',
     },
     delete       = {
-      hl = 'GitSignsDelete',
       text = icons.box.HeavyHorizontal,
-      numhl = 'GitSignsDeleteNr',
-      linehl = 'GitSignsDeleteLn',
     },
     topdelete    = {
-      hl = 'GitSignsDelete',
       text = '‾',
-      numhl = 'GitSignsDeleteNr',
-      linehl = 'GitSignsDeleteLn',
     },
     changedelete = {
-      hl = 'GitSignsChange',
       text = '~',
-      numhl = 'GitSignsChangeNr',
-      linehl = 'GitSignsChangeLn',
     },
   },
 }
