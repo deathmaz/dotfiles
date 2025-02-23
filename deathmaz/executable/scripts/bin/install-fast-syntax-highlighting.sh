9a289f73b5d6a66045a02e78edf37d6deb62d789
deathmaz <00maz1987@gmail.com>
2025-02-23 21:46:24 +0200

    refactor: --depth 1 for zsh syntax highlighting

--- deathmaz/executable/scripts/bin/install-fast-syntax-highlighting.sh
+++ deathmaz/executable/scripts/bin/install-fast-syntax-highlighting.sh
@@ -2,4 +2,4 @@
 
 cd $HOME/dotfiles/deathmaz/zsh/.zsh/plugins
 rm -rf fast-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
+git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting
