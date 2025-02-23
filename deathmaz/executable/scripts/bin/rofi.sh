2c8341b128f24c780e46eb33983a24ea517fae29
deathmaz <00maz1987@gmail.com>
2025-02-23 21:48:24 +0200

    feat: adapt rofi to work with `_xxx` scripts

--- deathmaz/executable/scripts/bin/rofi.sh
+++ deathmaz/executable/scripts/bin/rofi.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rofi -combi-modi window,drun -show combi
+rofi -combi-modi "window,nova:$MAZ_SCRIPTS_BIN/rofi-nova-script,drun,run" \
+  -modes "window,nova:$MAZ_SCRIPTS_BIN/rofi-nova-script,drun,run" \
+  -show combi
