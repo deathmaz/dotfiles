d0fadd53d9a362c41325d30ef1acabbbea2a8cbc
deathmaz <00maz1987@gmail.com>
2025-03-10 23:11:51 +0200

    feat: copy all needed ffmpeg executables for yt-dlp

--- deathmaz/executable/scripts/bin/download-ffmpeg-for-yt-dlp.sh
+++ deathmaz/executable/scripts/bin/download-ffmpeg-for-yt-dlp.sh
@@ -6,5 +6,5 @@ cd /tmp
 curl -L https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -O
 tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz
 mkdir -p $HOME/ffmpeg-for-yt-dlp
-cp -f /tmp/ffmpeg-master-latest-linux64-gpl/bin/ffmpeg $HOME/ffmpeg-for-yt-dlp/ffmpeg
+cp -f /tmp/ffmpeg-master-latest-linux64-gpl/bin/* $HOME/ffmpeg-for-yt-dlp/
 rm -rf ffmpeg-master-latest-linux64-gpl*
